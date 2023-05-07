ba10d71666d0f13115656aae2a9390ccaa6b3ff4
Shashank Dubey <allyouget14@gmail.com>
2023-05-07 22:09:32 +0530

    t

--- r1/src/App.js
+++ r1/src/App.js
@@ -2,31 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import login from './components/login';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  );
-}
-
-// function App(){
+// function App() {
 //   return (
-//     <div>
-//       <Routes>
-//         <Route path='/' element={
-//           <>
-//             <h1>Test</h1>
-//             <button><li><Link to="/about">About</Link></li></button>
-//           </>
-//         } exact />
-//         <Route path='/about' element={
-//           <h1>About Us</h1>
-//         } exact />
-//       </Routes>
+//     <div className="App">
+//       <h1>React App</h1>
 //     </div>
-//   )
+//   );
 // }
+
+function App(){
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={
+          <login />
+        } exact />
+        <Route path='/about' element={
+          <h1>About Us</h1>
+        } exact />
+      </Routes>
+    </div>
+  )
+}
 
 export default App;
